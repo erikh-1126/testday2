eff2143aa635f117de3ddec961ba37422e05514c
erikh-1126 <vuh6vr@virginia.edu>
2026-01-16 20:47:16 +0000

    Initial project environment setup

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 # %%
-import os
+import pandas as pd
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+columns = ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"]
+
+df = pd.read_csv(url, names=columns)
+df.head()
+
 # %%
